952ce7784d26c9070535ebd5723e8bcb8c0d78a6
channoh <channoh@p3.(none)>
2015-04-15 02:53:57 +0900

    4 bit full adder

--- fullAdder4/main.cpp
+++ fullAdder4/main.cpp
@@ -1,24 +1,24 @@
 #include "systemc.h"
-#include "fullAdder.h"
+#include "fullAdder4.h"
 #include "test.h"
 
 int sc_main(int argc, char* argv[]) {
-    sc_signal<sc_lv<1>> X_in,Y_in,Ci_in;
-    sc_signal<sc_lv<1>> S_out,C_out;
+    sc_signal< sc_lv<4> > X_in, Y_in, S_out;
+    sc_signal< sc_lv<1> > C_in, C_out;
 
     sc_clock CLK("clock",10,SC_NS);
 
-    fullAdder FA("FA");
-    FA(X_in,Y_in,Ci_in,S_out,C_out);
+    fullAdder4 FA4("FA4");
+    FA4(X_in, Y_in, C_in, S_out, C_out);
 
     test TEST("TEST");
-    TEST(CLK,X_in,Y_in,Ci_in,S_out,C_out);
+    TEST(CLK,X_in,Y_in,C_in,S_out,C_out);
 
     sc_trace_file *tf = sc_create_vcd_trace_file("wave");
     sc_trace(tf,CLK,"clock");
     sc_trace(tf,X_in,"X_in");
     sc_trace(tf,Y_in,"Y_in");
-    sc_trace(tf,Ci_in,"Ci_in");
+    sc_trace(tf,C_in,"C_in");
     sc_trace(tf,S_out,"S_out");
     sc_trace(tf,C_out,"C_out");
 
